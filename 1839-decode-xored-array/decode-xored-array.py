add397ea81eda240dc3a774c7ac2d297238443e4
aryan <aryannn053@gmail.com>
2024-09-29 08:28:40 +0530

    Time: 169 ms (21.38%) | Memory: 18.7 MB (6.31%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        arr = [first]
+
+        for i in range(len(encoded)):
+            arr.append(arr[i]^encoded[i])
+        
+        return arr
